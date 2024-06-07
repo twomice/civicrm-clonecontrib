f02dc7b968b8f3c4d53da916607fbd702ca043d6
Charlotte Jana <jana@systopia.de>
2024-06-07 14:41:47 +0200

    civix upgrade to 23.02.1

--- CRM/Clonecontrib/Upgrader.php
+++ CRM/Clonecontrib/Upgrader.php
@@ -5,7 +5,7 @@ use CRM_Clonecontrib_ExtensionUtil as E;
 /**
  * Collection of upgrade steps.
  */
-class CRM_Clonecontrib_Upgrader extends CRM_Clonecontrib_Upgrader_Base {
+class CRM_Clonecontrib_Upgrader extends CRM_Extension_Upgrader_Base {
   // By convention, functions that look like "function upgrade_NNNN()" are
   // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
 
